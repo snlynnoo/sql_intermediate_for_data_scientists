50427a65bb24e8e592fac116a562b1515aecf945
Sai Naing Lynn Oo <naing538@gmail.com>
2023-02-24 22:25:28 +0800

    challenge -2

--- Challenges.sql
+++ Challenges.sql
@@ -1,4 +1,4 @@
--- CHALLENGE 
+-- CHALLENGE (1) 
 
 -- Q: What function could you use to reformat a number from having eight dicimal places to two decimal places ?
 
@@ -12,3 +12,18 @@ SIMILAR TO
 -- What function would you use to measure the difference in charaters between two strings?
 
 LEVENSHTEIN()
+
+-- CHALLENGE (2)
+-- Write a query to return the count of employees in departments
+-- where the total salary in that dept. is greater than $ 5,000,000
+-- The result shoud be ordered from highest to lowest total salary
+
+SELECT	cd.department_name,
+		COUNT(e.*),
+		SUM(salary)
+FROM data_sci.employees e
+JOIN data_sci.company_departments cd
+ON e.department_id = cd.id
+GROUP BY cd.department_name
+HAVING SUM(salary) > 5000000
+ORDER BY SUM(e.salary) DESC;
